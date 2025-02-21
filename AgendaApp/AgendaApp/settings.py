d9897bd7fda6b9a320552826d5eff93b1fddd439
Ayodeji <amosayo16@gmail.com>
2025-02-21 11:43:54 +0100

    Setting up the Template side of things.

--- AgendaApp/AgendaApp/settings.py
+++ AgendaApp/AgendaApp/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "User.apps.UserConfig",
+    "Event.apps.EventConfig",
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,9 @@ ROOT_URLCONF = "AgendaApp.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [
+            BASE_DIR / "Templates",
+        ],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
